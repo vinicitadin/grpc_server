646f2f8667cdc329051a90348a2bf86a7958271b
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2024-12-06 14:15:27 -0300

    atualizado estrutura do projeto server

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,7 +9,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
-app.MapGrpcService<TimeService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
--- Server/Services/GreeterService.cs
+++ Server/Services/GreeterService.cs
@@ -18,5 +18,16 @@ namespace Server.Services
                 Message = "Hello " + request.Name
             });
         }
+
+        public override Task<TimeReply> GetTime(TimeRequest request, ServerCallContext context)
+        {
+            TimeReply output = new TimeReply();
+
+            output.Hour = 9;
+            output.Minute = 35;
+            output.Second = 10;
+
+            return Task.FromResult(output);
+        }
     }
 }
