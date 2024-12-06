805d2644dbc9fdc11a93fe4923af891b1d5ffc0b
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2024-12-06 09:39:42 -0300

    adicionado proto "time"

---
+++ Server/Services/TimeService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+
+namespace Server.Services
+{
+    public class TimeService : Timer.TimerBase
+    {
+
+        private readonly ILogger<TimeService> _logger;
+
+        public TimeService(ILogger<TimeService> logger) 
+        {
+            _logger = logger;
+        }
+
+        public override Task<TimeResponse> GetTime(TimeRequest request, ServerCallContext context)
+        {
+            TimeResponse output = new TimeResponse();
+
+            output.Hour = 9;
+            output.Minute = 35;
+            output.Second = 10;
+
+            return Task.FromResult(output);
+        }
+    }
+}
