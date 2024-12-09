8674b1f0772b2834d9109b02ec21fcde5574b134
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2024-12-09 10:10:29 -0300

    adicionado reflexão e web

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,11 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<GreeterService>().EnableGrpcWeb();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
